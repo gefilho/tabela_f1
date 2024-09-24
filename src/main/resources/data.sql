197014d0156e6de5074e7c8aa7d607f50f76b370
Geraldo Filho <gpsf888@gmail.com>
2024-09-24 14:24:54 -0300

    Ajuste de Corrida
    tabela corrida ajustada através de um data.sql, para que as corridas já estejam disponíveis previamente

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO corrida (nome, circuito, pais) VALUES ('GP do Brasil', 'Interlagos', 'Brasil');
+INSERT INTO corrida (nome, circuito, pais) VALUES ('GP de Mônaco', 'Monte Carlo', 'Mônaco');
+INSERT INTO corrida (nome, circuito, pais) VALUES ('GP da Itália', 'Monza', 'Itália');
